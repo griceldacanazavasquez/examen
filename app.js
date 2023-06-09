8fb131e794695823eee49968a9c7f35775686c3b
gricel <greys6416@gmail.com>
2023-06-09 19:40:47 -0300

    cambios en javascript

--- app.js
+++ app.js
@@ -1,8 +1,72 @@
 // objetos /variables
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 
-let product = [
-    {
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  checkInputs();
+});
 
+function checkInputs() {
+  const usernameValue = username.value;
+  const emailValue = email.value;
+  const passwordValue = password.value;
 
-}]
+  if (usernameValue === "") {
+    setErrorFor(username, "Se requiere nombre de usuario.");
+  } else {
+    setSuccessFor(username);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "correo electronico es requerido.");
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, "Por favor, insira um email válido.");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "se requiere contraseña.");
+  } else if (passwordValue.length < 7) {
+    setErrorFor(password, "La contraseña debe tener al menos 7 caracteres.");
+  } else {
+    setSuccessFor(password);
+  }
+  const formControls = form.querySelectorAll(".form-control");
+
+  const formIsValid = [...formControls].every((formControl) => {
+    return formControl.className === "form-control success";
+  });
+
+  if (formIsValid) {
+    console.log("¡El formulario es 100% válido!");
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+// Adiciona un mensaje de error
+  small.innerText = message;
+
+ // Adiciona una clase de error
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  // Añadir una clase de éxito
+  formControl.className = "form-control success";
+}
+//escaner a una red jacques 
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
